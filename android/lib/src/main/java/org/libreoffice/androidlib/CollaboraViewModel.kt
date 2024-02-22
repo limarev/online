06148927fc97c5663e5f8af3ef6ccb0c256fc50b
21204933 <AVlKostylev@sberbank.ru>
2024-02-22 14:46:43 +0700

    добавлена обработка эдит мода убраны элементы стандартного ui

--- android/lib/src/main/java/org/libreoffice/androidlib/CollaboraViewModel.kt
+++ android/lib/src/main/java/org/libreoffice/androidlib/CollaboraViewModel.kt
@@ -81,6 +81,9 @@ open class CollaboraViewModel(private val applicationContext: Context) : ViewMod
     protected val _asyncProcess = MutableStateFlow<AsyncProcessStatus?>(null)
     val asyncProcess: StateFlow<AsyncProcessStatus?> = _asyncProcess.asStateFlow()
 
+    protected val _isEditMode = MutableStateFlow<Boolean>(false)
+    val isEditMode: StateFlow<Boolean> = _isEditMode.asStateFlow()
+
     protected val _progressValue = Channel<Int>(Channel.UNLIMITED)
     val progressValue: Flow<Int> = _progressValue.receiveAsFlow()
 
@@ -403,10 +406,13 @@ open class CollaboraViewModel(private val applicationContext: Context) : ViewMod
             MSG_DIM_SCREEN,
             MSG_LIGHT_SCREEN,
             HIDEPROGRESSBAR,
-            MSG_EDITMODE,
             MSG_REQUESTFILECOPY,
             -> {
                 return false
+            }
+            MSG_EDITMODE -> {
+                _isEditMode.value = messageAndParameter[1] == "on"
+                return true
             }
         }
 
