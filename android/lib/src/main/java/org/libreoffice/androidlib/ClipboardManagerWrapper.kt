eaf30ab5ac77bc01aa21f6385143778fcd07656b
Arbonik <40275001+Arbonik@users.noreply.github.com>
2024-05-22 12:56:25 +0700

    Add cliclboardWrapper

---
+++ android/lib/src/main/java/org/libreoffice/androidlib/ClipboardManagerWrapper.kt
@@ -0,0 +1,41 @@
+package ru.tv1.android.tv
+
+import android.content.ClipData
+import android.content.ClipboardManager
+
+class ClipboardManagerWrapper(
+    private val clipboardManager : ClipboardManager
+){
+    private var cash : ClipData? = null
+
+    fun setClipData(clipData : ClipData){
+        cash = clipData
+        clipboardManager.setPrimaryClip(
+            ClipData.newPlainText(
+                clipboardExternallMessage,
+                clipboardExternallMessage
+            )
+        )
+    }
+
+    fun getClipData(): ClipData?{
+        val clipData = clipboardManager.primaryClip
+
+        if (cash == null) // если кеш пуст возвращаем как есть
+            return clipData
+
+        for (i in 0..(clipData?.itemCount ?: 0)){
+            val text = clipData?.getItemAt(i)?.text
+
+            // если скопирована заглушка и кеш не пуст возвращаем кеш
+            if (text == clipboardExternallMessage && cash != null)
+                return cash!!
+        }
+
+        return clipData
+    }
+
+    companion object {
+        private val clipboardExternallMessage = "Копирование запрещено!"
+    }
+}
